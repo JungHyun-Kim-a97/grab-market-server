04c12577e04c32762ede48842f7e94a9d2e86056
JungHyun-Kim-a97 <akdlswjf2@naver.com>
2021-02-25 03:24:29 +0900

    배너 API

--- server.js
+++ server.js
@@ -2,13 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 const port = 8090;
+
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2, //2개까지만 보여줌
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
 
 app.get("/products", (req, res) => {
   models.Product.findAll({
-    //findOnb을 사용하면 1개씩 검색 가능
+    //findOne을 사용하면 1개씩 검색 가능
     order: [["createdAt", "DESC"]], //order 정렬을 사용하여 최신순으로 나열
     attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"], //attributes를 주어서 원하는 컬럼만 검색가능
   })
@@ -20,20 +47,21 @@ app.get("/products", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("에러 발생");
+      res.status(400).send("에러 발생");
     });
 });
 app.post("/products", (req, res) => {
   const body = req.body;
-  const { name, description, price, seller } = body;
-  if (!name || !description || !price || !seller) {
-    res.send("모든 필드를 입력해주세요");
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
   }
   models.Product.create({
     name,
     description,
     price,
     seller,
+    imageUrl,
   })
     .then((result) => {
       console.log("상품 생성 결과 : ", result);
@@ -43,7 +71,7 @@ app.post("/products", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 업로드에 문제가 발생했습니다");
+      res.status(400).send("상품 업로드에 문제가 발생했습니다");
     });
 });
 
@@ -63,8 +91,17 @@ app.get("/products/:id", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 조회에 에러가 발생했습니다");
+      res.status(400).send("상품 조회에 에러가 발생했습니다");
     });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  //image 파일이 들어왔을 때 수행되는 로직
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path, //image의 위치
+  });
 });
 
 app.listen(port, () => {
